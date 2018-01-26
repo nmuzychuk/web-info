bea9179cc47ae1146d0d2190bf46d48033021b61
Nazarii Muzychuk <muzychuk.n@gmail.com>
2018-01-26 19:13:08 +0200

    Add a prototype

--- src/main/java/com/nmuzychuk/App.java
+++ src/main/java/com/nmuzychuk/App.java
@@ -1,10 +1,26 @@
 package com.nmuzychuk;
 
-/**
- * Hello world!
- */
+import spark.ModelAndView;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("greeting", "Web Info");
+            model.put("ip", req.ip());
+            model.put("userAgent", req.userAgent());
+
+            return render(model, "index");
+        });
+    }
+
+    private static String render(Map<String, Object> model, String templatePath) {
+        return new ThymeleafTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 }
