faad76afccfc2dc6be120bdc1e52860799852075
Nazarii Muzychuk <muzychuk.n@gmail.com>
2018-01-30 19:43:04 +0200

    Add additional info

--- src/main/java/com/nmuzychuk/App.java
+++ src/main/java/com/nmuzychuk/App.java
@@ -4,6 +4,7 @@ import spark.ModelAndView;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static spark.Spark.*;
@@ -12,9 +13,19 @@ public class App {
     public static void main(String[] args) {
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("greeting", "Web Info");
-            model.put("ip", req.ip());
-            model.put("userAgent", req.userAgent());
+            model.put("title", "Web Info");
+            model.put("heading", "Web Info");
+            model.put("property", "Property");
+            model.put("value", "Value");
+
+            Map<String, String> info = new LinkedHashMap<>();
+            info.put("Host", req.host());
+            info.put("IP", req.ip());
+            info.put("Protocol", req.protocol());
+            info.put("Accept-Language", req.headers("Accept-Language"));
+            info.put("User-Agent", req.userAgent());
+
+            model.put("info", info);
 
             return render(model, "index");
         });
